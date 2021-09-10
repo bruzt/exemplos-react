496b015f1b2dcdb4cba76d294c743190e34d2755
Bruno Zutim <bzutim@gmail.com>
2021-09-10 10:35:01 -0300

    refactor: style of buttons

--- slide-menu/src/components/HeaderCategories/index.tsx
+++ slide-menu/src/components/HeaderCategories/index.tsx
@@ -1,82 +1,83 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
-import { randomBytes } from 'crypto';
-import ClickAwayListener from 'react-click-away-listener';
+import React, { useEffect, useRef, useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import { randomBytes } from "crypto";
+import ClickAwayListener from "react-click-away-listener";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 const fakeCategories = [
-	{
-		id: 1,
-		name: 'Hardware',
-		parent_id: 0,
-	},
-	{
-		id: 2,
-		name: 'Periférico',
-		parent_id: 0,
-	},
-	{
-		id: 3,
-		name: 'Processador',
-		parent_id: 1,
-	},
-	{
-		id: 4,
-		name: 'Placa de Video',
-		parent_id: 1,
-	},
-	{
-		id: 5,
-		name: 'AMD',
-		parent_id: 3,
-	},
-	{
-		id: 6,
-		name: 'Intel',
-		parent_id: 3,
-	},
-	{
-		id: 7,
-		name: 'AMD',
-		parent_id: 4,
-	},
-	{
-		id: 8,
-		name: 'Nvidia',
-		parent_id: 4,
-	},
-	{
-		id: 9,
-		name: 'Joystick/Controle',
-		parent_id: 2,
-	},
-	{
-		id: 10,
-		name: 'Mouse/Teclado',
-		parent_id: 2,
-	},
+  {
+    id: 1,
+    name: "Hardware",
+    parent_id: 0,
+  },
+  {
+    id: 2,
+    name: "Periférico",
+    parent_id: 0,
+  },
+  {
+    id: 3,
+    name: "Processador",
+    parent_id: 1,
+  },
+  {
+    id: 4,
+    name: "Placa de Video",
+    parent_id: 1,
+  },
+  {
+    id: 5,
+    name: "AMD",
+    parent_id: 3,
+  },
+  {
+    id: 6,
+    name: "Intel",
+    parent_id: 3,
+  },
+  {
+    id: 7,
+    name: "AMD",
+    parent_id: 4,
+  },
+  {
+    id: 8,
+    name: "Nvidia",
+    parent_id: 4,
+  },
+  {
+    id: 9,
+    name: "Joystick/Controle",
+    parent_id: 2,
+  },
+  {
+    id: 10,
+    name: "Mouse/Teclado",
+    parent_id: 2,
+  },
 ];
 
 export default function Header() {
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+  const [menuIndex, setMenuIndex] = useState<number | undefined>(0);
+  const [categories, setCategories] = useState(fakeCategories);
+  const [JSXCategories, setJSXCategories] = useState<JSX.Element[] | undefined>(
+    []
+  );
 
-	const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
-	const [menuIndex, setMenuIndex] = useState<number | undefined>(0);
-	const [categories, setCategories] = useState(fakeCategories);
-	const [JSXCategories, setJSXCategories] = useState<JSX.Element[] | undefined>([]);
+  const menuIndexRef = useRef<number>();
+  menuIndexRef.current = menuIndex;
 
-	const menuIndexRef = useRef<number>();
-	menuIndexRef.current = menuIndex;
+  const JSXCategoriesRef = useRef<JSX.Element[]>();
+  JSXCategoriesRef.current = JSXCategories;
 
-	const JSXCategoriesRef = useRef<JSX.Element[]>();
-	JSXCategoriesRef.current = JSXCategories;
+  useEffect(() => {
+    createRoot();
+  }, []);
 
-	useEffect(() => {
-		createRoot();
-	}, []);
-
-	/*useEffect(() => {
+  /*useEffect(() => {
 		if(isSideMenuOpen == false){
 			setTimeout( () => {
 				setMenuIndex(0);
@@ -85,128 +86,133 @@ export default function Header() {
 		}
 	}, [isSideMenuOpen]);*/
 
-	function createRoot() {
+  function createRoot() {
+    // filtra as categorias sem pai
+    const root = categories.filter(
+      (category) => category.parent_id == 0 || category.parent_id == null
+    );
 
-		const root = categories.filter((category) => category.parent_id == 0 || category.parent_id == null);
+    const reactRoot = root.map((rootCategory) => {
+      // verifica se elas tem filhas
+      const childrens = categories.filter(
+        (category) => category.parent_id == rootCategory.id
+      );
+      const haveChildren = childrens.length > 0 ? true : false;
 
-		const reactRoot = root.map((rootCategory) => {
+      return (
+        <div key={rootCategory.id}>
+          <a>{rootCategory.name}</a>
+          {haveChildren && (
+            <button
+              type="button"
+              className="foward-button"
+              onClick={() => createChildren(rootCategory.id)}
+            >
+              <BsArrowRight size={24} />
+            </button>
+          )}
+        </div>
+      );
+    });
 
-			const childrens = categories.filter((category) => category.parent_id == rootCategory.id);
-			const haveChildren = childrens.length > 0 ? true : false;
+    const key = randomBytes(8).toString("hex");
 
-			return (
-				<div key={rootCategory.id}>
-					<span>{rootCategory.name}</span>
-					{haveChildren &&
-						(
-							<button
-								type='button'
-								className='foward-button'
-								onClick={() => createChildren(rootCategory.id)}
-							>
-								<BsArrowRight size={24} />
-							</button>
-						)
-					}
-				</div>
-			);
-		});
+    const menuSection = (
+      <div key={key} className="menu-section">
+        <div className="menu-section-title">
+          <h3>Categorias</h3>
+        </div>
+        {reactRoot}
+      </div>
+    );
 
-		const key = randomBytes(8).toString('hex');
+    setJSXCategories([menuSection]);
+  }
 
-		const menuSection = (
-			<div key={key} className="menu-section">
-				<div>
-					<h3>Categorias</h3>
-				</div>
-				{reactRoot}
-			</div>
-		);
+  function createChildren(fatherId: number) {
+    // filtra as categorias filhas
+    const children = categories.filter(
+      (category) => category.parent_id == fatherId
+    );
 
-		setJSXCategories([menuSection]);
-	}
+    const reactChildren = children.map((childCategory) => {
+      // verifica se elas tem filhas
+      const childChildrens = categories.filter(
+        (category) => category.parent_id == childCategory.id
+      );
+      const haveChildren = childChildrens.length > 0 ? true : false;
 
-	function createChildren(id: number) {
+      return (
+        <div key={childCategory.id}>
+          <a>{childCategory.name}</a>
+          {haveChildren && (
+            <button
+              type="button"
+              className="foward-button"
+              onClick={() => createChildren(childCategory.id)}
+            >
+              <BsArrowRight size={24} />
+            </button>
+          )}
+        </div>
+      );
+    });
 
-		const children = categories.filter((category) => category.parent_id == id);
+    const key = randomBytes(8).toString("hex");
 
-		const reactChildren = children.map((childCategory) => {
+    const menuSection = (
+      <div key={key} className="menu-section">
+        <div className="back-div">
+          <button type="button" className="back-button" onClick={backButton}>
+            <span>Voltar</span> <BsArrowLeft size={28} />
+          </button>
+        </div>
 
-			const childChildrens = categories.filter((category) => category.parent_id == childCategory.id);
-			const haveChildren = childChildrens.length > 0 ? true : false;
+        {reactChildren}
+      </div>
+    );
 
-			return (
-				<div key={childCategory.id}>
-					<span>{childCategory.name}</span>
-					{haveChildren && 
-						(
-							<button type='button' onClick={() => createChildren(childCategory.id)}>
-								<BsArrowRight size={24} />
-							</button>
-						)
-					}
-				</div>
-			);
-		});
+    const JSXCategoriesCopy = [...(JSXCategoriesRef.current as JSX.Element[])];
+    JSXCategoriesCopy.push(menuSection);
 
-		const key = randomBytes(8).toString('hex');
+    setJSXCategories(JSXCategoriesCopy);
+    setMenuIndex(Number(menuIndexRef.current) + 1);
+  }
 
-		const menuSection = (
-			<div key={key} className="menu-section">
-				<div className='back-div'>
-					<button
-						type='button'
-						className='back-button'
-						onClick={backButton}
-					>
-						<span>Voltar</span> <BsArrowLeft size={28} />
-					</button>
-				</div>
+  function backButton() {
+    setMenuIndex(Number(menuIndexRef.current) - 1);
 
-				{reactChildren}
-			</div>
-		);
+    setTimeout(() => {
+      const JSXCategoriesCopy = [
+        ...(JSXCategoriesRef.current as JSX.Element[]),
+      ];
+      JSXCategoriesCopy.pop();
+      setJSXCategories(JSXCategoriesCopy);
+    }, 300);
+  }
 
-		const JSXCategoriesCopy = [ ...JSXCategoriesRef.current as JSX.Element[] ];
-		JSXCategoriesCopy.push(menuSection);
+  return (
+    <Container>
+      <button type="button" onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}>
+        <GiHamburgerMenu size={30} />
+      </button>
 
-		setJSXCategories(JSXCategoriesCopy);
-		setMenuIndex(Number(menuIndexRef.current) + 1);
-	}
-
-	function backButton() {
-
-		setMenuIndex(Number(menuIndexRef.current) - 1);
-
-		setTimeout(() => {
-			const JSXCategoriesCopy = [ ...JSXCategoriesRef.current as JSX.Element[] ];
-			JSXCategoriesCopy.pop();
-			setJSXCategories(JSXCategoriesCopy);
-		}, 300);
-	}
-
-	return (
-		<Container>
-			<button
-				type='button'
-				onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}
-			>
-				<GiHamburgerMenu size={30} />
-			</button>
-
-			<ClickAwayListener onClickAway={() => { if(isSideMenuOpen) setIsSideMenuOpen(false) }}>
-				<div className={`side-menu-container ${isSideMenuOpen ? 'open' : ''}`}>
-
-						<div className="relative-menu">
-							<div className="absolute-menu" style={{ left: `-${Number(menuIndex) * 250}px` }}>
-
-								{JSXCategories}
-
-							</div>
-						</div>
-
-				</div>
-			</ClickAwayListener>
-		</Container>
-	);
+      <ClickAwayListener
+        onClickAway={() => {
+          if (isSideMenuOpen) setIsSideMenuOpen(false);
+        }}
+      >
+        <div className={`side-menu-container ${isSideMenuOpen ? "open" : ""}`}>
+          <div className="relative-menu">
+            <div
+              className="absolute-menu"
+              style={{ left: `-${Number(menuIndex) * 250}px` }}
+            >
+              {JSXCategories}
+            </div>
+          </div>
+        </div>
+      </ClickAwayListener>
+    </Container>
+  );
 }
--- slide-menu/src/components/HeaderCategories/styles.ts
+++ slide-menu/src/components/HeaderCategories/styles.ts
@@ -1,116 +1,150 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.header`
+  width: 100%;
+  height: 50px;
+
+  display: flex;
+
+  background: rgba(0, 0, 255, 0.5);
+
+  button {
+    font-size: 0;
+    align-self: center;
+
+    background: transparent;
+    border: 0;
+
+    padding: 5px;
+
+    cursor: pointer;
+  }
+
+  .side-menu-container {
+    position: fixed;
+    top: 50px;
+    left: calc(var(--side-menu-width) * -1);
+
+    width: var(--side-menu-width);
+    height: 100vh;
+
+    z-index: 10;
+
+    background: #eee;
+
+    overflow-x: hidden;
+
+    transition: 0.3s;
+
+    &.open {
+      left: 0px;
+    }
+  }
+
+  .side-menu-container .relative-menu {
+    position: relative;
+  }
+
+  .absolute-menu {
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+
+    display: flex;
+
+    transition: 0.3s;
+  }
+
+  .menu-section {
+    width: var(--side-menu-width);
+
+    flex-shrink: 0;
+  }
+
+  .menu-section .menu-section-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0;
+  }
+
+  .menu-section > div {
     width: 100%;
     height: 50px;
 
     display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    background: rgba(0, 0, 255, 0.5);
+    flex: 1;
 
-    button {
-        font-size: 0;
-        align-self: center;
+    border-bottom: 1px solid #111;
 
-        background: transparent;
-        border: 0;
+    padding: 0;
 
-        padding: 5px;
+    background: rgba(0, 0, 255, 0.1);
+  }
 
-        cursor: pointer;
+  .menu-section div span,
+  .menu-section div a {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+  }
+
+  .menu-section div a {
+    cursor: pointer;
+
+    padding-left: 30px;
+
+    &:hover {
+      background: rgba(0, 0, 255, 0.1);
+    }
+  }
+
+  .menu-section button {
+    width: 100%;
+    height: 100%;
+
+    border-radius: 0;
+
+    font-size: 16px;
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .menu-section button.foward-button:hover {
+    background: rgba(0, 0, 255, 0.1);
+  }
+
+  .menu-section button.foward-button:active {
+    background: rgba(0, 0, 255, 0.2);
+  }
+
+  .menu-section .back-div {
+    background: rgba(255, 0, 0, 0.1);
+    padding: 0;
+  }
+
+  .menu-section .back-button {
+    width: 100%;
+
+    padding: 0 40px 0 30px;
+
+    justify-content: center;
+
+    &:hover {
+      background: rgba(255, 0, 0, 0.1);
     }
 
-    .side-menu-container {
-        position: fixed;
-        top: 50px;
-        left: calc(var(--side-menu-width) * -1);
-
-        width: var(--side-menu-width);
-        height: 100vh;
-
-        z-index: 10;
-
-        background: #eee;
-        border: 1px solid #111;
-
-        overflow-x: hidden;
-
-        transition: .3s;
-
-        &.open {
-            left: 0px;
-        }
+    &:active {
+      background: rgba(255, 0, 0, 0.2);
     }
-
-    .side-menu-container .relative-menu {
-        position: relative;
-    }
-    
-    .absolute-menu {
-        width: 100%;
-        height: 100%;
-
-        position: absolute;
-
-        display: flex;
-
-        transition: 0.3s;
-    }
-    
-    .menu-section {
-        width: var(--side-menu-width);
-
-        flex-shrink: 0;
-    }
-
-    .menu-section >  div {
-        width: 100%;
-        height: 50px;
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        flex: 1;
-
-        border-bottom: 1px solid #111;
-
-        padding: 0 30px;
-
-        background: rgba(0, 0, 255, 0.1);
-    }
-
-    .menu-section div span {
-        width: 100%;
-        height: 100%;
-
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-    }
-    
-    .menu-section button {
-        width: 50%;
-        height: 100%;
-        
-        border: 0;
-        border-radius: 0;
-        
-        font-size: 16px;
-        
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
-
-    .menu-section .back-div {
-
-        background: rgba(255, 0, 0, 0.1);
-    }
-
-    .menu-section .back-button {
-        width: 100%;
-
-        justify-content: space-between;
-    }
+  }
 `;
